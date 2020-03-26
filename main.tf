be3390bd0667c8ebbf808f7de868af9537c43b83
Victor Godinez <victor.godinez@digitalonus.com>
2020-03-26 07:26:11 -0600

    Add another security group

--- main.tf
+++ main.tf
@@ -26,6 +26,31 @@ resource "aws_security_group" "vhugo-sg" {
   }
 }
 
+# This was added by another user
+resource "aws_security_group" "another-sg" {
+  name = "another-sg"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+#=====================================
 terraform {
   backend "s3" {
     bucket         = "my-backend-vhugo"
